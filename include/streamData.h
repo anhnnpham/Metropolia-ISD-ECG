cdd26c5c998808ae6534a68522bae7399d3aa319
Anh Pham <anhnnpham@gmail.com>
2020-03-08 18:57:53 +0200

    TCP socket config

--- include/streamData.h
+++ include/streamData.h
@@ -1,6 +1,20 @@
 #ifndef ECG_ISD_ESP32_STREAMDATA_H
 #define ECG_ISD_ESP32_STREAMDATA_H
 
+// C headers
+extern "C"
+{
+#include <lwip/sockets.h>
+#include <esp_log.h>
+#include <string.h>
+#include <errno.h>
+#include "sdkconfig.h"
+}
+
+#define PORT_NUMBER 8001
+
+static char tag[] = "socket_server";
+
 class StreamData {
 public:
 	StreamData();
@@ -9,3 +23,4 @@ public:
 };
 
 #endif
+
